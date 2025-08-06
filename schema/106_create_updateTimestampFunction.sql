8294cf58b0a5aeafd95105886da5c53bd6a1ff85
Jose Ortiz <joseortizgarciaq@gmail.com>
2025-08-06 18:20:43 -0400

    updatedTimestampFunction.sql file created.

---
+++ schema/106_create_updateTimestampFunction.sql
@@ -0,0 +1,8 @@
+-- Function to update the updated_at timestamp
+CREATE OR REPLACE FUNCTION update_updated_at_column()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = NOW();
+    RETURN NEW;
+END;
+$$ language 'plpgsql';
